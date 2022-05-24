ed6fe94805d260869801970341568d4fef636608
SurazGaire <luxheartsuraz@gmail.com>
2022-05-24 15:13:18 +0545

    feat : Adding blog using token

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,11 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({}).populate("notes", { content: 1, date: 1 });
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+  });
 
   response.json(users);
 });
@@ -17,15 +21,14 @@ usersRouter.post("/", async (request, response) => {
       error: "username must be unique",
     });
   }
-  if (password.length < 4 || username.length < 4) {
-    return response.status(400).json({
-      error: "either username or password is less than 3 characters",
-    });
-  }
 
   if (!password || !username) {
     return response.status(400).json({
       error: "username and password should be given",
+    });
+  } else if (password.length < 4 || username.length < 4) {
+    return response.status(400).json({
+      error: "either username or password is less than 3 characters",
     });
   }
 
--- tests/helping_test.js
+++ tests/helping_test.js
@@ -32,8 +32,13 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON());
 };
 
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((user) => user.toJSON());
+};
 module.exports = {
   initialBlogs,
   nonExistingId,
   blogsInDb,
+  usersInDb,
 };

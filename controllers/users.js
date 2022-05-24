99a5c3bec961d190b64d46f98463b6d02c221866
SurazGaire <luxheartsuraz@gmail.com>
2022-05-24 13:19:35 +0545

    feat : Adding new users in a database

---
+++ controllers/users.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("notes", { content: 1, date: 1 });
+
+  response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json({
+      error: "username must be unique",
+    });
+  }
+  if (password.length < 4 || username.length < 4) {
+    return response.status(400).json({
+      error: "either username or password is less than 3 characters",
+    });
+  }
+
+  if (!password || !username) {
+    return response.status(400).json({
+      error: "username and password should be given",
+    });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
